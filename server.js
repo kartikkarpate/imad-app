fcfe7dc2a75ff9277f053659c334967144c253ce
KARTIK KARPATE <kartikrkarpate@gmail.com>
2017-08-18 21:56:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ res.sendFile(path.join(__dirname, 'ui', 'about.html'));
     
 });
 
-app.get('/main.js', function (req, res){
+app.get('/ui/main.js', function (req, res){
 res.sendFile(path.join(__dirname, 'ui', 'main.js'));
     
 });
